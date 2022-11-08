d9a8ac3210a67e98dc40d68c8b319000aecb732b
Pablo M. (Bublo) <pablitoyjoseito@hotmail.es>
2022-11-08 19:50:34 +0100

    Fixed double click and added informative text.

--- mscrolldown_sim.cpp
+++ mscrolldown_sim.cpp
@@ -2,12 +2,16 @@
 #include<iostream>
 
 int main(){
+    //Mouse input to be simulated. In this case, mouse wheel down.
     INPUT mscroll_down[1] = {};
     mscroll_down[0].type = INPUT_MOUSE;
     mscroll_down[0].mi.mouseData = -1;
     mscroll_down[0].mi.dwFlags = MOUSEEVENTF_WHEEL;
+
+    std::cout << "Hold down your mouse wheel to simulate scrolling your mouse wheel down.\nIt goes really fast, so be careful.\nTo stop the program, simply close this window";
+    //Mouse input simulation
     while(true){
-        if(GetKeyState(0x04) == -128){
+        if(GetKeyState(0x04) & 0x8000){
             SendInput(1,mscroll_down,sizeof(INPUT));
         }
     }
